869d45109dc34e18fd9f194158b74e7e82abe6ea
omartinezr87-commits <omartinezr87@gmail.com>
2026-04-15 13:14:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'impostor-v2';
+const CACHE = 'impostor-v3';
 const ASSETS = ['/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
